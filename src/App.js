ae375582aaf7ca87e8af0eae751f685b4608531b
aminhb <aminhbaieb90@gmail.com>
2021-03-09 11:16:29 +0100

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import imageInSrc from "./imageInSrc.jpg";
+import "./styles.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div style={{ border: "solid 1px black", maxWidth: "100 vw" }}>
+            <h2 className="title red">Your name here</h2>
+            <br />
+            <h3 className="title1 red">Hbaieb Amin</h3>
+            {/*image in src*/}
+            <img className="myimg" src={imageInSrc} alt="my_pic" />
+            <br />
+            {/*image in pub*/}
+            <img src="/GMClogo.png" alt="my_pic2" />
+            <video width="320" height="240" controls>
+                <source src="/myVideo.mp4" type="video/mp4" />
+            </video>
+        </div>
+    );
 }
 
 export default App;
